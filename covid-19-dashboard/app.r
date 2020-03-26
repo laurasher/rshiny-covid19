c6de8c3b8aec8150911616a1b97c68b4865c468b
Laura Asher <laura.asher@jhuapl.edu>
2020-03-26 15:33:58 -0400

    Mirrordom coming along.

--- covid-19-dashboard/app.r
+++ covid-19-dashboard/app.r
@@ -20,24 +20,24 @@ if(!require(shinyWidgets)) install.packages("shinyWidgets", repos = "http://cran
 # Case data
 numHeadCols = 4
 numTailCols = 2
-covid_alpha = 0.5
+covid_alpha = 0.7
 deaths_col = "#cc4c02"
 #covid_col = "#D98D09"
 covid_col = "#a20f0f"
 recovered_col = "#70a801"
 
-confirmed <- as.data.frame(data.table::fread("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"))
-confirmed <- confirmed[,1:ncol(confirmed)-1]
+confirmed <- as.data.frame(data.table::fread("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/archived_data/archived_time_series/time_series_19-covid-Confirmed_archived_0325.csv"))
+#confirmed <- confirmed[,1:ncol(confirmed)-1]
 confirmed$type <- 'confirmed'
 confirmed$plotColor <- covid_col
 
-deaths <- as.data.frame(data.table::fread("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"))
-deaths <- deaths[,1:ncol(deaths)-1]
+deaths <- as.data.frame(data.table::fread("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/archived_data/archived_time_series/time_series_19-covid-Deaths_archived_0325.csv"))
+#deaths <- deaths[,1:ncol(deaths)-1]
 deaths$type <- 'deaths'
 deaths$plotColor <- deaths_col
 
-recovered <- as.data.frame(data.table::fread("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv"))
-colnames(recovered) <- c("Province/State", "Country/Region", "Lat", "Long", "1/22/20", "1/23/20", "1/24/20", "1/25/20", "1/26/20", "1/27/20", "1/28/20", "1/29/20", "1/30/20", "1/31/20", "2/1/20", "2/2/20", "2/3/20", "2/4/20", "2/5/20", "2/6/20", "2/7/20", "2/8/20", "2/9/20", "2/10/20", "2/11/20", "2/12/20", "2/13/20", "2/14/20", "2/15/20", "2/16/20", "2/17/20", "2/18/20", "2/19/20", "2/20/20", "2/21/20", "2/22/20", "2/23/20", "2/24/20", "2/25/20", "2/26/20", "2/27/20", "2/28/20", "2/29/20", "3/1/20" ,"3/2/20", "3/3/20", "3/4/20", "3/5/20", "3/6/20", "3/7/20", "3/8/20", "3/9/20", "3/10/20", "3/11/20", "3/12/20", "3/13/20", "3/14/20", "3/15/20", "3/16/20", "3/17/20", "3/18/20", "3/19/20", "3/20/20", "3/21/20", "3/22/20", "3/23/20", "3/24/20")
+recovered <- as.data.frame(data.table::fread("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/archived_data/archived_time_series/time_series_19-covid-Recovered_archived_0325.csv"))
+#colnames(recovered) <- c("Province/State", "Country/Region", "Lat", "Long", "1/22/20", "1/23/20", "1/24/20", "1/25/20", "1/26/20", "1/27/20", "1/28/20", "1/29/20", "1/30/20", "1/31/20", "2/1/20", "2/2/20", "2/3/20", "2/4/20", "2/5/20", "2/6/20", "2/7/20", "2/8/20", "2/9/20", "2/10/20", "2/11/20", "2/12/20", "2/13/20", "2/14/20", "2/15/20", "2/16/20", "2/17/20", "2/18/20", "2/19/20", "2/20/20", "2/21/20", "2/22/20", "2/23/20", "2/24/20", "2/25/20", "2/26/20", "2/27/20", "2/28/20", "2/29/20", "3/1/20" ,"3/2/20", "3/3/20", "3/4/20", "3/5/20", "3/6/20", "3/7/20", "3/8/20", "3/9/20", "3/10/20", "3/11/20", "3/12/20", "3/13/20", "3/14/20", "3/15/20", "3/16/20", "3/17/20", "3/18/20", "3/19/20", "3/20/20", "3/21/20", "3/22/20", "3/23/20", "3/24/20")
 recovered$type <- 'recovered'
 recovered$plotColor <- recovered_col
 combined <- rbind(confirmed, deaths, recovered)
@@ -45,33 +45,41 @@ combined <- rbind(confirmed, deaths, recovered)
 
 cases <- as.data.frame(data.table::fread("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/web-data/archived_data/data/cases.csv"))
 
-ui <- fluidPage(theme = shinytheme("slate"),
+ui <- fluidPage(
   fluidRow(tags$head(includeCSS("styles.css")),
            tags$head(tags$style(".leaflet-control-zoom { display: none; }")),
            tags$head(tags$style(".leaflet-control-attribution { display: none; }")),
-    column(2,
-           span(h3("Total Confirmed", align = "center"), style="color:white"),
-           span(h1(textOutput("reactive_case_count"), align = "center"), style="color:#e60000"),
-           #plotOutput("countPlot", height="430px", width="100%"),
-           pickerInput("outcome_select", width="60%",
-                       choices = c("Confirmed cases", "Deaths", "Recovered"), 
-                       selected = c("Cases"),
-                       multiple = FALSE),
-           sliderInput("day", label = "Days since outbreak", min = 1, max = ncol(combined)[1]-(numHeadCols+numTailCols),
-                       value = ncol(combined)[1]-(numHeadCols+numTailCols)-1, step = 1, width = "100%",
-                       animate = animationOptions(interval = 700, loop = FALSE))
-    ),
-    column(6,
-           leafletOutput("COVID19", height="800px")
-    ),
-    column(2,
-           span(h3("Total Deaths", align = "center"), style="color:white"),
-           span(h1(textOutput("reactive_death_count"), align = "center"), style="color:white")),
-    column(2,
-           span(h3("Total Recovered", align = "center"), style="color:white"),
-           span(h1(textOutput("reactive_recovered_count"), align = "center"), style="color:#70a801"))
-  )
-)
+      column(2,
+             span(p("Total Confirmed", align = "center"), style="font-size:20px;text-align:center;color:white"),
+             span(p(textOutput("reactive_case_count"), align = "center"), style="font-weight:500;font-size:48px;color:#e60000;text-align:center;"),
+             tableOutput('table'),
+             #plotOutput("countPlot", height="430px", width="100%"),
+             pickerInput("outcome_select", width="80%",
+                         choices = c("Confirmed cases", "Deaths", "Recovered"), 
+                         selected = c("Cases"),
+                         multiple = FALSE),
+             sliderInput("day", label = "Days since outbreak", min = 1, max = ncol(combined)[1]-(numHeadCols+numTailCols),
+                         value = ncol(combined)[1]-(numHeadCols+numTailCols)-1, step = 1, width = "100%",
+                         animate = animationOptions(interval = 700, loop = FALSE))
+      ),
+      column(6,
+             leafletOutput("COVID19", height="800px")
+      ),
+      fluidRow(
+        column(4,
+          column(4,
+                 span(p("Total Deaths", align = "center"), style="font-size:20px;text-align:center;color:white"),
+                 span(p(textOutput("reactive_death_count"), align = "center"), style="font-weight:500;font-size:48px;text-align:center;color:white"),
+                 tableOutput('deaths_table')),
+          column(4,offset = 2,
+                 span(p("Total Recovered", align = "center"), style="font-size:20px;text-align:center;color:white"),
+                 span(p(textOutput("reactive_recovered_count"), align = "center"), style="font-weight:500;font-size:48px;text-align:center;color:#70a801"),
+                 tableOutput('recovered_table'))
+          ),
+        column(4, span(p("Global confirmed cases", align = "left"), style="font-size:12px;text-align:center;color:white"),
+               plotOutput("epiCurve", height="430px", width="430px"),)
+      )
+))
 
 server <- function(input, output, session) {
   
@@ -108,7 +116,7 @@ server <- function(input, output, session) {
       'plotColor' = combined$plotColor
       )) %>%
       addProviderTiles(providers$CartoDB.DarkMatter, options = providerTileOptions(noWrap = TRUE)) %>%
-      fitBounds(~min(long), ~min(lat), ~max(long), ~max(lat)) %>% setView(0, 0, zoom = 2) %>%
+      fitBounds(~min(long), ~min(lat), ~max(long), ~max(lat)) %>% setView(0, 0, zoom = 2.1) %>%
       addCircleMarkers(radius = ~3*(log10(as.numeric(caseCnt))),
                              stroke = FALSE,
                              #fillOpacity = ~(caseCnt)*.5,
@@ -119,10 +127,39 @@ server <- function(input, output, session) {
   output$countPlot <- renderPlot({
     grouped_combined <- filteredData()  %>% group_by(country) %>% summarise_if(is.numeric, sum)
     ggplot(data.frame(
-      'country' = grouped_combined$country,
+      'ountry' = grouped_combined$country,
       'caseCnt' = grouped_combined %>% select(colnames(grouped_combined)[length(colnames(grouped_combined))]) %>% pull()
     ),aes_string(x='country', y='caseCnt')) + 
       geom_bar(stat = "identity") + theme(axis.text.x=element_blank()) + theme_minimal() + coord_flip() + scale_y_log10() + labs(x = "", y = "")
+  })
+  
+  output$epiCurve <- renderPlot({
+    ggplot(data = data.frame("date" = as.Date(colnames(confirmed[8:ncol(confirmed)-(numTailCols+1)]), format='%m/%d/%y'), 
+                             "y" = data.frame(colSums(confirmed[8:ncol(confirmed)-(numTailCols+1)])) %>% pull() ), aes(x=date, y=y)) + 
+      geom_line(colour="#ffaa00", size=1.0) +
+      geom_point(colour="#ffaa00", size=3, shape=21, fill="#ffaa00") + labs(x = "", y = "Cumulative confirmed cases") +
+      theme_minimal() + theme(panel.grid.minor = element_blank(), 
+                              panel.grid.major = element_blank(),
+                              panel.background = element_blank(),
+                              plot.background = element_blank()
+      )}, bg="transparent")
+  
+  output$table <- renderTable({
+    grouped_by_country <- confirmed %>% group_by(`Country/Region`) %>% summarise_if(is.numeric, sum)
+    grouped_by_country <- data.frame("Country"= grouped_by_country$`Country/Region`, "Confirmed" = grouped_by_country[ncol(grouped_by_country)] %>% pull())
+    grouped_by_country[order(-grouped_by_country$Confirmed),]
+  })
+  
+  output$deaths_table <- renderTable({
+    grouped_by_country <- deaths %>% group_by(`Country/Region`) %>% summarise_if(is.numeric, sum)
+    grouped_by_country <- data.frame("Country"= grouped_by_country$`Country/Region`, "Deaths" = grouped_by_country[ncol(grouped_by_country)] %>% pull())
+    grouped_by_country[order(-grouped_by_country$Deaths),]
+  })
+  
+  output$recovered_table <- renderTable({
+    grouped_by_country <- recovered %>% group_by(`Country/Region`) %>% summarise_if(is.numeric, sum)
+    grouped_by_country <- data.frame("Country"= grouped_by_country$`Country/Region`, "Recovered" = grouped_by_country[ncol(grouped_by_country)] %>% pull())
+    grouped_by_country[order(-grouped_by_country$Recovered),]
   })
   
   #Reactive total case count
